46131afc7262d9c8b48a7b11f7c12058cf7d1033
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2022-04-06 15:09:36 +1000

    Added javascript to highlight page link

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,22 +6,33 @@ class Navbar extends HTMLElement {
     /** Runs everytime element is inserted into document object model. */
     connectedCallback() {
         this.innerHTML = `
-      <header>
-      <nav>
-        <a class="home-link" href="index.html">
-          <img class="home-logo" src="../images/golfball-50px.svg" alt="Golf ball">
-          <h1>Mighty Mini Golf</h1>
-        </a>
+        <header>
+        <nav>
+          <a class="home-link" href="index.html">
+            <img class="home-logo" src="../images/golfball-50px.svg" alt="Golf ball">
+            <h1>Mighty Mini Golf</h1>
+          </a>
 
-        <ul>
-          <li><a href="about.html"><h4>About</h4></a></li>
-          <li><a href="food.html"><h4>Food and Drink</h4></a></li>
-          <li><a href="contact.html"><h4>Contact</h4></a></li>
-          <li><a href="book.html"><h4>Book</h4></a></li>
-        </ul>
-      </nav>
-      </header>
-    `;
+          <ul>
+            <li><a id="about-link" href="about.html"><h4>About</h4></a></li>
+            <li><a id="food-link" href="food.html"><h4>Food and Drink</h4></a></li>
+            <li><a id="contact-link" href="contact.html"><h4>Contact</h4></a></li>
+            <li><a id="book-link" href="book.html"><h4>Book</h4></a></li>
+          </ul>
+        </nav>
+        </header>
+      `;
+      
+      let page = document.URL;
+      if (page.includes("about")) {
+        document.getElementById("about-link").style.color = "white";
+      } else if (page.includes("food")) {
+        document.getElementById("food-link").style.color = "white";
+      } else if (page.includes("contact")) {
+        document.getElementById("contact-link").style.color = "white";
+      } else if (page.includes("book")) {
+        document.getElementById("book-link").style.color = "white";
+      } 
     }
   }
 
