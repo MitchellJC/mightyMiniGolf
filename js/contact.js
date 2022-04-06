a33f3f0a054b8e4115a6865af9302ff357822f1b
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2022-04-06 21:04:47 +1000

    Created js to respond to contact submission

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("contact-form");
+const formHeight = form.getBoundingClientRect()["height"];
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    /** Collect form information into dictionary */
+    let userInfo = {};
+    for (field of form.elements) {
+        userInfo[field.id] = field.value;
+    }
+
+    form.innerHTML= 
+    `<p>
+        Thank you for contacting us ${userInfo["contact-fname"]}, 
+        one of our friendly team members will get back to you shortly. 
+    </p>
+    <ul class="contact-submit-list">
+        <h3>Submitted Contact Details:</h3>
+        <li><b>Your First Name:</b> ${userInfo["contact-fname"]}</li>
+        <li><b>Your Last Name:</b> ${userInfo["contact-lname"]}</li>
+        <li><b>Your Email:</b> ${userInfo["contact-email"]}</li>
+        <li><b>Your Phone:</b> ${userInfo["contact-phone"]}</li>
+    </ul>`;
+    form.style.minHeight = `${formHeight}px`;
+});
