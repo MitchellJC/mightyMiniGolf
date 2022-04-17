5bafae47808eeddc63b9b9d1da266d821434f34d
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2022-04-17 14:53:59 +1000

    Animated faq drop down box

--- js/about.js
+++ js/about.js
@@ -1,48 +1,18 @@
 (function(){
 
-    // CSS constants.
-    const BLOCK = "block"
-    const NONE = "None"
+    const faqs = document.getElementById("faq-list")
+                         .getElementsByTagName("li");
 
-    const LI = "li"
+    for (const faq of faqs) {
+        const question = faq.getElementsByTagName("h3")[0];
+        const answer = faq.getElementsByClassName("answer-box")[0];
 
-    // Page text constants.
-    const FAQ_1 = "Can I get a refund or change my booking?";
-    const FAQ_2 = "faq_2";
-    const ANSWER_1 = 
-    `You can get a refund or change your
-    booking upto 5 days before the date.`;
-    const ANSWER_2 = 
-    `Ans2`;
-
-    const faqs = [FAQ_1, FAQ_2]
-    const answers = [ANSWER_1, ANSWER_2]
-    const faqHtmlList = document.getElementById("faq-list");
-
-    faqsLength = faqs.length
-    for (let questionNum = 1; questionNum <= faqsLength; questionNum++) {
-        let question = faqs[questionNum - 1];
-        let answer = answers[questionNum - 1];
-
-        let htmlQuestion = document.createElement(LI);
-        let htmlAnswer = document.createElement(LI);
-
-        htmlQuestion.id = `faq${questionNum}`;
-        htmlAnswer.id = `faq-answer${questionNum}`;
-
-        htmlQuestion.innerText = question;
-        htmlAnswer.innerText = answer;
-
-        htmlAnswer.style.display = NONE;
-
-        faqHtmlList.appendChild(htmlQuestion);
-        faqHtmlList.appendChild(htmlAnswer);
-
-        htmlQuestion.addEventListener('click', (_)=>{
-            if (htmlAnswer.classList.contains("hidden")) {
-                htmlAnswer.style.dis
+        question.addEventListener("click", (event) => {
+            answer.classList.add("faq-answer")
+            if (answer.classList.contains("answer-box-open")) {
+                answer.classList.remove("answer-box-open");
             } else {
-                
+                answer.classList.add("answer-box-open");
             }
         });
     }
