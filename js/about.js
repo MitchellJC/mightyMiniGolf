4af9a7dce2e5748451c3e2d1e4df2177f2f81085
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2022-04-18 15:28:22 +1000

    Refactored css

--- js/about.js
+++ js/about.js
@@ -1,13 +1,13 @@
 (function(){
-
     const faqs = document.getElementById("faq-list")
                          .getElementsByTagName("li");
 
+    // Connect each faq to open its relevant answer                     
     for (const faq of faqs) {
         const question = faq.getElementsByTagName("h3")[0];
         const answer = faq.getElementsByClassName("answer-box")[0];
 
-        question.addEventListener("click", (event) => {
+        question.addEventListener("click", (_) => {
             answer.classList.add("faq-answer")
             if (answer.classList.contains("answer-box-open")) {
                 answer.classList.remove("answer-box-open");
