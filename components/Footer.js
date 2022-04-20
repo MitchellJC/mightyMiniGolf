3ac3818cb6608185be9583e30ebade60f652f88d
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2022-04-20 20:52:37 +1000

    Made navbar responsive
    and footer no longer grid

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,18 @@ const FOOTER_HTML =
 <footer>
 <div class="content-container">
 <ul>
-  <li><a href="index.html">Home Page</a></li>
-  <li><a href="about.html">About Us</a></li>
-  <li><a href="book.html">Book</a></li>
-  <li><a href="contact.html">Contact Us</a></li>
-  <li><a href="food.html">Food and Drink</a></li>
-  <li><a href="terms.html">Terms and Conditions</a></li>
-  <li><a href="about.html#faq-section">FAQs</a></li>
-  <li><a href="about.html#covid-section">Covid-19 Health and Safety</a></li>
+  <div>
+    <li><a href="index.html">Home Page</a></li>
+    <li><a href="about.html">About Us</a></li>
+    <li><a href="book.html">Book</a></li>
+    <li><a href="contact.html">Contact Us</a></li>
+    <li><a href="food.html">Food and Drink</a></li>
+    <li><a href="terms.html">Terms and Conditions</a></li>
+  </div>
+  <div>
+    <li><a href="about.html#faq-section">FAQs</a></li>
+    <li><a href="about.html#covid-section">Covid-19 Health and Safety</a></li>
+  </div>
 </ul>
 </div>
 </footer>
