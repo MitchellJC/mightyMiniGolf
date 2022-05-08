30dec6d5925f3a09491bfe17f86a3a6c6ccf715b
jubilee67 <81349046+jubilee67@users.noreply.github.com>
2022-05-08 21:34:44 +1000

    Implemented automatic photo slideshow

---
+++ js/index.js
@@ -0,0 +1,70 @@
+(function(){
+    const UNSELECTED_DOT = "&#9899";
+    const SELECTED_DOT = "&#9898";
+    const SLIDESHOW_CHANGE_PERIOD =  10000;
+
+    const leftSlideshowArrow = document.getElementById("left-slideshow-arrow");
+    const rightSlideshowArrow = document.getElementById("right-slideshow-arrow");
+    const dots = document.getElementsByClassName("slideshow-dot");
+    const slideshowImages = document.getElementsByClassName("slideshow-photo");
+
+    /** Changes the current image on the slideshow to the 
+     * image corresponding to imageNum.
+     * 
+     * @param {number} imageNum - Number to change image to.
+     */
+    function changeSlideShowImage (imageNum) {
+        // Make the last slideshow image connect back to first.
+        slideshowImagesEndIndex = slideshowImages.length - 1;
+        if (imageNum > slideshowImagesEndIndex) {
+            imageNum = 0;
+        } else if (imageNum < 0) {
+            imageNum = slideshowImagesEndIndex;
+        }
+
+        let currentDot = dots[imageNum]
+        let currentImage = slideshowImages[imageNum]
+        currentImageNum = imageNum;
+
+        // Hide all images.
+        for (const image of slideshowImages) {
+            image.classList.add("hidden");
+        }
+        // Convert all dots to unselected dot.
+        for (const dot of dots) {
+            dot.innerHTML= UNSELECTED_DOT;
+        }
+        currentDot.innerHTML = SELECTED_DOT;
+        currentImage.classList.remove("hidden"); 
+    }
+
+    function resetSlideShowTime(autoSlideShowEvent) {
+        clearInterval(autoSlideShowEvent);
+        newAutoSlideShowEvent = setInterval(() => {changeSlideShowImage(currentImageNum + 1)}, 
+            SLIDESHOW_CHANGE_PERIOD);
+        return newAutoSlideShowEvent;
+    }
+
+    let currentImageNum = 0;
+    let autoSlideShowEvent = setInterval(() => {changeSlideShowImage(currentImageNum + 1)}, 
+        SLIDESHOW_CHANGE_PERIOD);
+
+    // Make clicking dots change slideshow image.
+    for (let index = 0; index < dots.length; index++) {
+        let dot = dots[index];
+
+        dot.addEventListener("click", () => {
+            autoSlideShowEvent = resetSlideShowTime(autoSlideShowEvent);
+            changeSlideShowImage(index);
+        });
+    }
+
+    leftSlideshowArrow.addEventListener("click", () => {
+        autoSlideShowEvent = resetSlideShowTime(autoSlideShowEvent);
+        changeSlideShowImage(currentImageNum - 1);
+    });
+    rightSlideshowArrow.addEventListener("click", () => {
+        autoSlideShowEvent = resetSlideShowTime(autoSlideShowEvent);
+        changeSlideShowImage(currentImageNum + 1);
+    });
+})();
